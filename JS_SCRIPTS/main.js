6abf653e2ffd800554107a16584bd7265aaa0fd4
ogboNoble001 <ogbonoble84@gmail.com>
2025-09-24 07:26:28 +0100

    update

--- JS_SCRIPTS/main.js
+++ JS_SCRIPTS/main.js
@@ -6,26 +6,33 @@ window.addEventListener('DOMContentLoaded', () => {
         const mainBody = document.querySelector('.mainBody');
         const sidebar = document.querySelector('.sidebar');
         const sidebarOverlay = document.querySelector('.sidebar-overlay');
-        const icns = document.querySelectorAll('.icns');
-
-icns.forEach((icn) => {
-        icn.addEventListener('click', () => {
-                // First remove 'active' from all icons
-                icns.forEach((otherIcn) => {
-                        otherIcn.classList.remove('active');
+        
+        const topNavIcns = document.querySelectorAll('.topNav div');
+        
+        topNavIcns.forEach((icn) => {
+                icn.addEventListener('click', () => {
+                        topNavIcns.forEach((otherIcn) => {
+                                otherIcn.classList.remove('active');
+                        });
+                        icn.classList.add('active');
                 });
-                // Then add 'active' to the clicked icon
-                icn.classList.add('active');
         });
-});
-      
+        
+        const icns = document.querySelectorAll('.icns');
+        icns.forEach((icn) => {
+                icn.addEventListener('click', () => {
+                        icns.forEach((otherIcn) => {
+                                otherIcn.classList.remove('active');
+                        });
+                        icn.classList.add('active');
+                });
+        });
+        
         setTimeout(() => {
                 splash.style.display = 'none';
                 nav.style.display = 'flex';
                 mainBody.style.display = 'flex';
                 sidebar.style.display = 'block';
                 sidebarOverlay.style.display = 'block';
-                
-
         }, 1050);
 });
